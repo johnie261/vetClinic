42e3ca8116158959990154466ad86d86795c2eeb
johnie261 <njorogejohn261@gmail.com>
2023-05-03 21:21:27 +0300

    Create table owners

--- schema.sql
+++ schema.sql
@@ -1,13 +1,23 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT, 
-    name VARCHAR(20), 
-    date_of_birth DATE, 
-    escape_attempts INT, 
-    neutered BOOLEAN, 
-    weight_kg DECIMAL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    species VARCHAR(50)
 );
+
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+-- create table owners
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INT
+);
+
 
