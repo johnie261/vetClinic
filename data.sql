4070925591c66feb1f1f8fb09688c32aa819e83d
johnie261 <njorogejohn261@gmail.com>
2023-05-04 21:29:15 +0300

    Insert data into vets table

--- data.sql
+++ data.sql
@@ -91,3 +91,11 @@ SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
 WHERE name = 'Boarmon';
 
 
+
+-- Insert data into vets table
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
+
