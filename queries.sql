0c700dee569c4508f1514cbd3873f2cb7d36a8aa
John Njoroge <njorogejohn261@gmail.com>
2023-05-09 00:17:27 +0300

    Update queries

--- queries.sql
+++ queries.sql
@@ -177,8 +177,10 @@ JOIN species s ON a.species_id = s.id
 WHERE ve.name = 'Maisy Smith'
 GROUP BY s.name;
 
-
+-- Use explain analyze to check execution time
 explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT * FROM visits where vet_id = 2;
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
 
